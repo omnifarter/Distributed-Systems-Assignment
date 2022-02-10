88cab26fbcdd890a3f28fcdfd3ba0dc36b87f68d
Gabriel <gabrielphang@gmail.com>
2022-02-10 15:31:25 +0800

    chore: clean part 1 and added HW pdf

--- Pset/Pset1/Pset1.go
+++ Pset/Pset1/Pset1.go
@@ -3,14 +3,22 @@ package Pset1
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
+/*
+	Start the simulation with the required number of clients.
+	WaitGroup is used here to keep the simulation running indefinitely.
+*/
 func Part1() {
 	NUMBER_OF_CLIENTS := 4
-
 	wg := sync.WaitGroup{}
 	wg.Add(1)
+
 	fmt.Println("Simulating client server architecture...")
+	fmt.Println("Press Ctrl + c to stop program execution.")
+
+	time.Sleep(time.Second * 2)
 
 	server, err := StartServer(NUMBER_OF_CLIENTS)
 
@@ -22,6 +30,10 @@ func Part1() {
 	for i := 0; i < len(server.clientsReceiveChannel); i++ {
 		StartClient(i, server.clientsSendChannel[i], server.clientsReceiveChannel[i])
 	}
+
 	wg.Wait()
+}
+
+func Part2() {
 
 }
